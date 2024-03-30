2358857051e8126fd12e19cf86e6aaaadc36b74d
zohaib055 <zohaib055@gmail.com>
2024-03-31 03:55:06 +0500

    home feed

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,3 +1,4 @@
+import HomeFeed from "pages/home";
 import IRoute from "../interfaces/route";
 import LoginPage from "../pages/login";
 
@@ -6,6 +7,11 @@ const routes: IRoute[] = [
     path: "/",
     name: "Login",
     component: LoginPage,
+  },
+  {
+    path: "/home-feed",
+    name: "Home Feed",
+    component: HomeFeed,
   },
   
 ];
