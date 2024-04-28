c2d1c6ee2f51189ace573afea07d0b61018f8839
angel <aadcantu@gmail.com>
2024-04-27 23:23:15 -0500

    Removed callback import.

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
 
 const AudioPlayer = ({ link, id, setPlaying, playerType, volume}) => {
