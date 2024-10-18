36483a8c682c3680d86f8edb56021ecdadc999ed
Ehsanullah Qadeer <eehsanullah232@gmail.com>
2024-10-19 02:55:39 +0500

    add wait message page

--- src/pages/creator/registeration/component/Thankyou.tsx
+++ src/pages/creator/registeration/component/Thankyou.tsx
@@ -11,44 +11,46 @@ const Thankyou: React.FC<ThankyouProps> = ({
 }) => {
   const navigate = useNavigate();
   const handleButton = () => {
-    navigate("/home");
+    navigate("/login");
   };
   return (
     <>
-      <div className=" rotate-[deg] absolute top-6 left-[-560px] ">
-        <img src={sharpline} alt="" />
-      </div>
-      <div className="flex text-center gap-5 flex-col justify-center items-center w-1/2">
-        <h1 className="text-[34px] font-semibold text-[#FFFFFF] tracking-[-1.36px] px-8  ">
-          Thank you for joining the{" "}
-          <span className="text-[#9EFF00]">MVSSIVE </span>Community!
-        </h1>
-        <p className="text-[#b2b2b2]">
-          Your application has been received and is currently under review. You
-          will receive an email notification once your account has been approved
-          and is ready for use. if you have any questions or need assistance,
-          please contact our{" "}
-          <span className="text-[#9EFF00]">support team.</span>{" "}
-        </p>
+      <div className="flex text-center gap-5 flex-col h-screen overflow-hidden justify-center items-center fixed w-screen">
+        <div className="flex text-center gap-5 flex-col h-screen overflow-hidden justify-center items-center w-1/2">
+          <div className=" rotate-[deg] absolute top-6 overflow-hidden left-[-560px] ">
+            <img src={sharpline} alt="" />
+          </div>
+          <h1 className="text-[34px] font-semibold text-[#FFFFFF] tracking-[-1.36px] px-8  ">
+            Thank you for joining the{" "}
+            <span className="text-[#9EFF00]">MVSSIVE </span>Community!
+          </h1>
+          <p className="text-[#b2b2b2]">
+            Your application has been received and is currently under review.
+            You will receive an email notification once your account has been
+            approved and is ready for use. if you have any questions or need
+            assistance, please contact our{" "}
+            <span className="text-[#9EFF00]">support team.</span>{" "}
+          </p>
 
-        <button
-          onClick={handleButton}
-          className={`bg-[#9EFF00] rounded-full text-sm px-5 py-3  justify-center items-center font-semibold ${
-            submittedApplication ? "hidden" : "flex"
-          }`}
-        >
-          Back to Home
-        </button>
-        <button
-          className={`bg-[#9EFF00] rounded-full text-sm px-5 py-3  justify-center items-center font-semibold ${
-            !submittedApplication ? "hidden" : "flex"
-          }`}
-        >
-          Re-send email
-        </button>
-      </div>
-      <div className=" rotate-[300deg] absolute top-56 right-[-470px] ">
-        <img src={sharpline} alt="" />
+          <button
+            onClick={handleButton}
+            className={`bg-[#9EFF00] rounded-full text-sm px-5 py-3  justify-center items-center font-semibold ${
+              submittedApplication ? "hidden" : "flex"
+            }`}
+          >
+            Back to Home
+          </button>
+          <button
+            className={`bg-[#9EFF00] rounded-full text-sm px-5 py-3  justify-center items-center font-semibold ${
+              !submittedApplication ? "hidden" : "flex"
+            }`}
+          >
+            Re-send email
+          </button>
+          <div className=" rotate-[300deg] absolute top-56 right-[-470px] overflow-hidden ">
+            <img src={sharpline} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );
