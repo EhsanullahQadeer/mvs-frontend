a00287113ddb0e795cfa81cd5fd22f958aa25721
Roberto Rivas <32427856+Roberri898@users.noreply.github.com>
2024-10-24 00:10:17 -0500

    Matched the mocks on functionality

--- src/pages/settings/content-management/components/ComposerDialog.tsx
+++ src/pages/settings/content-management/components/ComposerDialog.tsx
@@ -49,7 +49,7 @@ function ComposerDialog(props: Props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [isInvite, setIsInvite] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [selected,setSelected] = useState([]);
+  const [selected, setSelected] = useState(null);
   // Debounce the search value
   const debouncedSearchValue = useDebounce(searchTerm, 300);
 
@@ -59,7 +59,7 @@ function ComposerDialog(props: Props) {
     setOpenComposerDialog(false);
     setIsInvite(false);
     setSearchTerm("");
-    setSelected([]);
+    setSelected(null);
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,7 +77,6 @@ function ComposerDialog(props: Props) {
             professionalName: debouncedSearchValue,
             take: 10,
           });
-          // console.log("list response with serach", response.data);
           setSearchResults(response.data.users);
         } catch (error) {
           console.error("Error fetching data:", error);
@@ -91,37 +90,29 @@ function ComposerDialog(props: Props) {
   }, [debouncedSearchValue]);
 
   const handleButtonClick = () => {
-    if (selected.length > 0) {
-      selected.forEach((composer)=>handleAddComposer(composer));
-      handleClose();
+    if (selected) {
+      handleAddComposer(selected);
+      setSelected(null);
+      setSearchTerm("");
     }
     if (isInvite) {
       console.log("search term", searchTerm);
     }
   };
 
-  const isNotSelected = (selectedComposer) => {
-    for (const a of selected) {
-      if (a.id === selectedComposer.id){
-        return false;
-      }
-    }
+  const isSelected = (selectedComposer) => {
     for (const a of contributors) {
-      if (a.id === selectedComposer.id){
-        return false;
+      if (a.id === selectedComposer.id) {
+        return true;
       }
     }
-    return true;
-  };
-
-  const handleDeleteSelectedContributor = (index) => {
-    const newSelected  = selected.filter((s,i) => i !== index);
-    setSelected(newSelected);
+    return false;
   };
 
   const handleSelectingContributor = (selectedComposer) => {
-    if (isNotSelected(selectedComposer)) {
-      setSelected([...selected,selectedComposer]);
+    if (!isSelected(selectedComposer)) {
+      setSelected(selectedComposer);
+      setSearchTerm(selectedComposer.professional_name);
     }
   };
 
@@ -141,7 +132,7 @@ function ComposerDialog(props: Props) {
             padding: "12px",
             color: "#E5E5E5",
             width: "500px",
-            overflow: "hidden",
+            overflow: "visible",
           },
         }}
       >
@@ -162,69 +153,121 @@ function ComposerDialog(props: Props) {
           </IconButton>
         </div>
 
-        <div className="py-4 border-t border-b border-eclipseGray flex gap-3 w-full items-stretch">
-          <div className="flex-1 relative">
-            <input
-              type="text"
-              placeholder="Search collaborators"
-              className="px-4 py-3 text-sm font-normal text-coolGray w-full bg-jetBlack rounded-lg border border-eclipseGray hover:border-secondaryBlue focus:border-transparent focus:outline-secondaryBlue focus:outline-2 focus:outline-offset-0"
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-            <div className="absolute right-[9px] top-1/2 -translate-y-1/2 text-[#4C4C4C] cursor-pointer flex">
-              {loading && (
-                <CircularProgress style={{ color: "#C4FF48" }} size={20} />
-              )}
+        <div className="py-4 border-t border-b border-eclipseGray flex flex-col gap-3 w-full items-stretch relative">
+          <div className="flex gap-3">
+            <div className="flex-1 relative">
+              <input
+                type="text"
+                placeholder="Search collaborators"
+                className="px-4 relative py-3 text-sm font-normal text-coolGray w-full bg-jetBlack rounded-lg border border-eclipseGray hover:border-secondaryBlue focus:border-transparent focus:outline-secondaryBlue focus:outline-2 focus:outline-offset-0"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              <div className="absolute right-[9px] top-1/2 -translate-y-1/2 text-[#4C4C4C] cursor-pointer flex">
+                {loading && (
+                  <CircularProgress style={{ color: "#C4FF48" }} size={20} />
+                )}
+              </div>
+            </div>
+
+            <div
+              className={`${
+                selected
+                  ? "bg-[#059669] text-softGray cursor-pointer"
+                  : "bg-eclipseGray text-dimGray pointer-events-none"
+              } rounded-lg text-sm font-semibold w-[69px] flex justify-center items-center`}
+              onClick={handleButtonClick}
+            >
+              {isInvite ? "Invite" : "Add"}
             </div>
           </div>
-
           <div
-            className={`${
-              selected.length > 0
-                ? "bg-[#059669] text-softGray cursor-pointer"
-                : "bg-eclipseGray text-dimGray pointer-events-none"
-            } rounded-lg text-sm font-semibold w-[69px] flex justify-center items-center`}
-            onClick={handleButtonClick}
+            className={`flex flex-col bg-eerieBlack absolute top-full w-full rounded-lg ${dropdownItemMaxHeight} overflow-y-auto custom-dropdown`}
           >
-            {isInvite ? "Invite" : "Add"}
+            {searchResults.map((composer, idx) => {
+              const { thumbnail, professional_name, primary_label, sub_label } =
+                composer;
+              return (
+                <div
+                  onClick={() => {
+                    handleSelectingContributor(composer);
+                  }}
+                  key={professional_name + idx}
+                  className={`px-2.5 py-${searchedContributorItemYPadding} cursor-pointer flex gap-2.5 hover:bg-darkGray rounded`}
+                >
+                  <div
+                    className={`w-${thumbnailSize} h-${thumbnailSize} rounded-full`}
+                  >
+                    <img
+                      src={thumbnail}
+                      alt="thumbnail"
+                      className="w-full h-full object-cover rounded-full"
+                    />
+                  </div>
+
+                  <div className="flex-1 flex justify-between items-center">
+                    <div>
+                      <div className="flex items-center">
+                        <span className="text-sm font-semibold text-white">
+                          {professional_name}
+                        </span>
+
+                        {isOwner && (
+                          <span className="ml-1.5 px-1.5 bg-eerieBlack rounded-md">
+                            You
+                          </span>
+                        )}
+                      </div>
+
+                      <div className="text-sm font-normal text-dimGray flex gap-1">
+                        {`${primary_label} / ${sub_label}`}
+                      </div>
+                    </div>
+                    {isOwner && (
+                      <div className="text-coolGray text-xs font-normal">
+                        Owner
+                      </div>
+                    )}
+                  </div>
+                  {isSelected(composer) ? (
+                    <div className="text-coolGray text-xs font-normal content-center">
+                      Already added
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              );
+            })}
           </div>
         </div>
 
         <div className="flex flex-col gap-1 overflow-hidden">
           <div className="text-silver text-xs font-normal p-2">
             Contributors to this sample
-            <div>
-              {
-              selected.map((composer,idx) => {
-                const { professional_name } = composer;
-                return (
-                <div
-                  key={professional_name+idx+selected}
-                  onClick={() =>handleDeleteSelectedContributor(idx)}
-                >
-                  {professional_name}
-                </div>)
-              }
-              )}
-            </div>
           </div>
-          <div className={`bg-eclipseGray rounded-lg ${dropdownItemMaxHeight} p-${searchedContributorsPadding} overflow-y-auto custom-dropdown`}>
-            {searchResults.length ? (
-              searchResults.map((composer, idx) => {
-                const { thumbnail, professional_name, primary_label, sub_label } =
-                  composer;
+          <div
+            className={`bg-eclipseGray rounded-lg ${dropdownItemMaxHeight} p-${searchedContributorsPadding} overflow-y-auto custom-dropdown`}
+          >
+            {contributors.length ? (
+              contributors.map((composer, idx) => {
+                const {
+                  thumbnail,
+                  professional_name,
+                  primary_label,
+                  sub_label,
+                } = composer;
                 return (
                   <div
-                    onClick={() => {
-                      handleSelectingContributor(composer);
-                    }}
-                    key={professional_name + idx}
-                    className={`px-2.5 py-${searchedContributorItemYPadding} cursor-pointer flex gap-2.5 hover:bg-gunMetal rounded`}
+                    key={"contributor" + idx}
+                    className={`px-2.5 py-${searchedContributorItemYPadding} flex gap-2.5 rounded`}
                   >
-                    <div className={`w-${thumbnailSize} h-${thumbnailSize} rounded-full`}>
+                    <div
+                      className={`w-${thumbnailSize} h-${thumbnailSize} rounded-full`}
+                    >
                       <img
                         src={thumbnail}
-                        alt="thumbnail"
+                        alt={`${professional_name} thumbnail`}
                         className="w-full h-full object-cover rounded-full"
                       />
                     </div>
