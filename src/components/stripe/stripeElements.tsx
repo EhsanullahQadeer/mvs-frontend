3c3900d03a50067cd89032025baa74e5abd73269
Roberto R <roberri898@gmail.com>
2025-03-24 13:31:24 -0500

    get customer session

--- src/components/stripe/stripeElements.tsx
+++ src/components/stripe/stripeElements.tsx
@@ -3,19 +3,36 @@ import {loadStripe, StripeElementsOptions} from '@stripe/stripe-js';
 import {config} from 'config/ConfigManager';
 import DemoStripe from './demoStripe';
 
+import axios from '../../api/axios';
+import { useState, useEffect } from 'react';
+
+
 const stripePromise = loadStripe(config.get('STRIPE.PUBLISHABLE_KEY'));
 
-function StripeElements() {
-  const options: StripeElementsOptions = {
-    mode: 'payment' as const,
-    amount: 1099,
-    currency: 'usd',
-    paymentMethodCreation: 'manual',
+function StripeElements() { 
+  const [customerSession, setCustomerSession] = useState(null);
+  async function getCustomerSession() {
+    const customerSession = await axios.post('stripe/create-customer-session');
+    setCustomerSession(customerSession.data);
   }
+  useEffect(() => {
+    getCustomerSession();
+  }, []);
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <DemoStripe />
-    </Elements>
+    <>
+      {customerSession ? (
+        <Elements stripe={stripePromise} options={{
+          mode: 'payment' as const,
+          amount: 1099,
+          currency: 'usd',
+          customerSessionClientSecret: customerSession?.clientSecret,
+        }}>
+          <DemoStripe />
+        </Elements>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
   );
 }
 
