d3fcc8a67d80ae06c7f5390945a1c670f81437df
Karla-zamora <karla.zamora01@utrgv.edu>
2025-03-26 19:46:07 -0500

    push profile content when opening chatbox

--- src/pages/profile/components/ProfileAboutSection.tsx
+++ src/pages/profile/components/ProfileAboutSection.tsx
@@ -182,7 +182,7 @@ const ProfileAboutSection = (props: Props) => {
 
   if (showChat && activeConversation) {
     return (
-      <div className="fixed right-0 top-[70px] w-[500px] h-[calc(100vh-70px)] bg-richBlack overflow-hidden z-50">
+      <div className="h-[calc(100vh-70px)] bg-richBlack overflow-hidden z-50 animate-slide-in w-full">
         <ConversationProvider>
           <Chatbox
             onClose={() => {setShowChat(false); setChatOpen(false)}}
