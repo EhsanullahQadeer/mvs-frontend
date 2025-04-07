4f80f318fef24ee3b4105aca8f507aff2cc04664
Roberto R <roberri898@gmail.com>
2025-04-07 15:06:22 -0500

    Used path-to-regexp to get the username

--- src/utils/authHandler.ts
+++ src/utils/authHandler.ts
@@ -1,5 +1,6 @@
 import publicRoutes from "routes/publicRoutes";
 import cookie from "js-cookie";
+import {match} from 'path-to-regexp'
 
 export const isRouteAccessible = (): boolean => {
     const token = cookie.get("token");
@@ -8,22 +9,20 @@ export const isRouteAccessible = (): boolean => {
     if (token) return true;
 
     return publicRoutes.some((route) =>
-        new RegExp(`^${route.path.replace(/:([a-zA-Z]+)/g, "([^/]+)")}$`).test(currentPath)
+        !!match(route.path)(currentPath)
     );
 };
 
 export const shouldRedirectToProfile = (path: string): { shouldRedirect: boolean; username: string | null } => {
     const token = cookie.get("token");
     
-    // Only check for the specific username route pattern
     if (path === "/:username") {
         const currentPath = window.location.pathname;
-        const usernameMatch = currentPath.match(/^\/([a-zA-Z0-9_-]{3,30})$/);
-        
-        if (token && usernameMatch) {
+        const matchs = match("/:username")(currentPath);
+        if (token && matchs) {
             return {
                 shouldRedirect: true,
-                username: usernameMatch[1]
+                username: matchs.params.username as string
             };
         }
     }
