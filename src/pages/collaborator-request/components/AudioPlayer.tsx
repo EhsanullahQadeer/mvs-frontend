4005812fde72a0c33f75da4252b9e4c5be836662
EhsanullahQadeer <eehsanullah232@gmail.com>
2025-04-14 05:58:57 +0500

    Add audio player component with waveform visualization using WaveSurfer.js

--- src/pages/collaborator-request/components/AudioPlayer.tsx
+++ src/pages/collaborator-request/components/AudioPlayer.tsx
@@ -14,30 +14,34 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioUrl }) => {
     if (waveformRef.current) {
       wavesurfer.current = WaveSurfer.create({
         container: waveformRef.current,
-        waveColor: "#363A3F", // Unplayed wave color
-        progressColor: "#D0D8E3", // Played wave color
+        waveColor: "#363A3F",
+        progressColor: "#D0D8E3",
         cursorColor: "transparent",
-        barWidth: 2,
-        barGap: 1,
-        height: 177,
+        barWidth: 3,
+        barGap: 3,
+        height: 88,
         barRadius: 0,
         normalize: true,
         backend: "WebAudio",
+        fillParent: true,
+        minPxPerSec: 1,
+        interact: true,
+        hideScrollbar: true,
+        barAlign: 'bottom'
       });
 
       wavesurfer.current.load(audioUrl);
 
-      // Handle play/pause when clicking on the waveform
       wavesurfer.current.on("interaction", () => {
         wavesurfer.current?.playPause();
       });
-
-      return () => {
-        if (wavesurfer.current) {
-          wavesurfer.current.destroy();
-        }
-      };
     }
+
+    return () => {
+      if (wavesurfer.current) {
+        wavesurfer.current.destroy();
+      }
+    };
   }, [audioUrl]);
 
   return (
@@ -49,7 +53,15 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioUrl }) => {
       }}
       className="pt-[80px] md:pt-[120px] rounded-lg w-full"
     >
-      <div ref={waveformRef} className="w-full cursor-pointer" />
+      <div className="relative">
+        <div className="h-[177px] relative">
+          <div 
+            ref={waveformRef} 
+            className="absolute bottom-0 left-0 right-0 [&>wave]:absolute [&>wave]:bottom-0" 
+          />
+        </div>
+        <div className="absolute bottom-0 left-0 right-0 h-[1px] bg-[#B2B2B2]" />
+      </div>
     </div>
   );
 };
